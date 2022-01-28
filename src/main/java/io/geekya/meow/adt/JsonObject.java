b0bee2d99cb994a592cdf1d058809c2ea2e74d4e
Lin Yang <geekya215@gmail.com>
2022-01-28 09:42:51 +0800

    update ADT class

--- src/main/java/io/geekya/meow/adt/JsonObject.java
+++ src/main/java/io/geekya/meow/adt/JsonObject.java
@@ -3,9 +3,9 @@ package io.geekya.meow.adt;
 import java.util.Map;
 
 public final class JsonObject extends JsonValue {
-    private final Map<JsonString, JsonValue> value;
+    private final Map<String, JsonValue> value;
 
-    public JsonObject(Map<JsonString, JsonValue> value) {
+    public JsonObject(Map<String, JsonValue> value) {
         this.value = value;
     }
 
